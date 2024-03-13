5f0c8d0486688ccc9b7e7e5a372777ec21b774fb
Sushant <cashu853@gmail.com>
2024-03-13 12:09:30 +0530

    Weather APP

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,34 +1,14 @@
-// const API_KEY="37e4710446e86b83da991bdc7bcab3f1"
-// const BASE_URL="https://api.openweathermap.org/data/2.5"
+const API_KEY="37e4710446e86b83da991bdc7bcab3f1"
+const BASE_URL="https://api.openweathermap.org/data/2.5"
 
 
-// const getWeatherData =(infoType, searchParam)=>{
-//    const url = new URL(BASE_URL +  "/" + infoType)
-//    url.search= new URLSearchParams({...searchParam, addid:API_KEY});
+const getWeatherData =(infoType, searchParam)=>{
+   const url = new URL(BASE_URL +  "/" + infoType)
+   url.search= new URLSearchParams({...searchParam, addid:API_KEY});
 
-//    return fetch(url)
-//    .then((res)=>res.json())
-//    .then((data)=>data)
-// } ;
+   return fetch(url)
+   .then((res)=>res.json())
+   .then((data)=>data)
+} ;
 
-// export default  getWeatherData;
-
-
-
-
-import { useState, useEffect } from "react";
-
-function WeatherServise(city){
-   const [data, setData] = useState({})
-   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q={city}&appid=37e4710446e86b83da991bdc7bcab3f1")
-    .then((res)=>res.json())
-    .then((res)=>setData(res[city]))
-   }, [city])
-   
-   console.log(data)
-   return data
-}
-
-
-export default WeatherServise;
+export default  getWeatherData;
