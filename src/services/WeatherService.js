e8a74e6ac851866fb0d95dda8d61c89a6e12c535
Sushant <cashu853@gmail.com>
2024-03-14 11:56:57 +0530

    Weather App

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -20,50 +20,57 @@ const getWeatherData = async (infoType, searchParam) => {
 const formatCurrentWeather = (data) => {
   const {
     coord: { lat, lon },
+
     main: { temp, feels_like, temp_min, temp_max, humidity },
     name,
     dt,
-    sys: { country, sunrise, sunset }, // Fixed typo in sunset
+    sys: { country, sunrise, sumset },
     weather,
     wind: { speed }
-  } = data;
+  } = data
 
-  const { main: details, icon } = weather[0];
+  const { main: details, icon } = weather[0]
 
-  return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed };
-};
+  return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sumset, details, icon, speed }
+}
 
-const formatDailyHourlyWeather = (data, timezone) => {
-  let { daily, hourly } = data;
-  
-  const formatToLocalTime = (secs, format="cccc, dd LLL yyyy' | Local time: 'hh:mm a") =>
-    DateTime.fromSeconds(secs).setZone(timezone).toFormat(format);
 
-  daily = daily.slice(1, 6).map(d => ({
-    title: formatToLocalTime(d.dt),
-    temp: d.temp.day,
-    icon: d.weather[0].icon
-  }));
+const formatForecastWeather = (data)=>{
+  let { timezone, daily, hourly}  = data;
+  daily= daily.slice(1,6).map(d =>{
+    return{
+      title: formatToLocalTime(d.dt, timezone, 'ccc'),
+      temp: d.temp.day,
+      icon: d.weather[0].icon
+    }
+  })
 
-  hourly = hourly.slice(1, 6).map(h => ({
-    title: formatToLocalTime(h.dt, 'hh:mm a'),
-    temp: h.temp,
-    icon: h.weather[0].icon
-  }));
+  hourly= daily.slice(1,6).map(d =>{
+    return{
+      title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+      temp: d.temp.day,
+      icon: d.weather[0].icon
+    }
+  })
 
-  return { daily, hourly };
-};
+  return{timezone, daily, hourly}
+}
 
-const getFormattedWeatherData = async (searchParam) => {
-  const formattedCurrentWeather = await getWeatherData("weather", searchParam).then(formatCurrentWeather);
+const getFormatedWeatherData = async (searchParam) => {
+  const formatedCurrentWeather = await getWeatherData("weather", searchParam)
+    .then(formatCurrentWeather);
 
-  const { lat, lon } = formattedCurrentWeather;
 
-  const formattedForecastWeather = await getWeatherData('forecast', {
-    lat, lon, exclude: 'current,minutely,alerts', units: searchParam.units
-  }).then(data => formatDailyHourlyWeather(data, searchParam.timezone));
+  const { lat, lon } = formatedCurrentWeather
 
-  return { ...formattedCurrentWeather, ...formattedForecastWeather };
-};
+  const formatedForecastWeather = await getWeatherData('oncall', {
+    lat, lon, exclude: 'current, minutely, alerts', units: searchParam.units
+  }).then(formatForecastWeather)
 
-export default getFormattedWeatherData;
+  return {...formatedCurrentWeather, ...formatedForecastWeather}
+}
+
+
+const formatToLocalTime = (secs, zone, format="cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format)
+
+export default getFormatedWeatherData;
